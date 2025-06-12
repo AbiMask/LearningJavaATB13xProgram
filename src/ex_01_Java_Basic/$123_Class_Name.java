292efebda48dff345a6d19a85b93bbb23bcf38a0
AbiMask <abisubimask@gmail.com>
2025-06-13 00:27:03 +0530

    June 13

---
+++ src/ex_01_Java_Basic/$123_Class_Name.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class $123_Class_Name {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm your class with name $123");
+    }
+}
---
+++ src/ex_01_Java_Basic/__.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basic;
+
+public class __ {
+    public static void main(String[] args) {
+        System.out.println("Hi, I'm with name __");
+    }
+}
