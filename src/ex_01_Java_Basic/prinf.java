d992d6ee168a0cc54ad5e0fe305faa8ec4289d74
AbiMask <abisubimask@gmail.com>
2025-06-12 23:29:05 +0530

    June 9

---
+++ src/ex_01_Java_Basic/prinf.java
@@ -0,0 +1,47 @@
+package ex_01_Java_Basic;
+
+public class prinf {
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 1;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println(" ");
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+    }
+}
