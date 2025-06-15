2a0f394bda89295be62fc0fe0866f100354f5f2f
AbiMask <abisubimask@gmail.com>
2025-06-15 09:34:46 +0530

    June 15

--- src/ex06_UserInput/Lab_UserInput_Scanner_01.java
+++ src/ex06_UserInput/Lab_UserInput_Scanner_01.java
@@ -1,4 +1,18 @@
 package ex06_UserInput;
 
+import java.util.Scanner;
+
 public class Lab_UserInput_Scanner_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the age");
+
+        int age = scanner.nextInt();
+        System.out.println("I captured your input");
+
+        System.out.println("Your age is "+age);
+
+        String canIVote = age < 18? "no" : "yes";
+        System.out.println(canIVote);
+    }
 }
--- src/ex06_UserInput/Lab_UserInput_Scanner_02.java
+++ src/ex06_UserInput/Lab_UserInput_Scanner_02.java
@@ -4,15 +4,37 @@ import java.util.Scanner;
 
 public class Lab_UserInput_Scanner_02 {
     public static void main(String[] args) {
+        System.out.println("Requesting User to Enter all inputs");
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the age");
 
-        int age = scanner.nextInt();
-        System.out.println("I captured your input");
+        System.out.print("Enter Byte Value: ");
+        byte b = scanner.nextByte();
 
-        System.out.println("Your age is "+age);
+        System.out.print("Enter Short Value: ");
+        short s = scanner.nextShort();
 
-        String canIVote = age < 18? "no" : "yes";
-        System.out.println(canIVote);
+        System.out.print("Enter Int Value: ");
+        int i = scanner.nextInt();
+
+        System.out.print("Enter Long Value: ");
+        long l = scanner.nextLong();
+
+//        System.out.print("Enter Char Value: ");
+//
+
+        System.out.print("Enter Float Value: ");
+        float f = scanner.nextFloat();
+
+        System.out.print("Enter Double Value: ");
+        double d  = scanner.nextDouble();
+
+        System.out.print("Enter Boolean Value: ");
+        boolean b1 = scanner.nextBoolean();
+
+        System.out.print("Enter String Value: ");
+        String s1 = scanner.next();
+
+        System.out.println(b+" "+s+" "+i+" "+l+" "+d+" "+f+" "+b1+" "+s1);
+
     }
 }
