fe2de5618f242cab5e03495e2a0170a9742c31ce
AbiMask <abisubimask@gmail.com>
2025-06-16 10:40:22 +0530

    June 16

---
+++ src/ex_09_Switch/Lab091_Interview.java
@@ -0,0 +1,12 @@
+package ex_09_Switch;
+
+public class Lab091_Interview {
+    public static void main(String[] args) {
+        long a11 = 30l;
+        switch ((int) a11) {
+            case 30:
+                System.out.println("Hi, I'm 30th number");
+                break;
+        }
+    }
+}
