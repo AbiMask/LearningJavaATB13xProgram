61d8e96a8a8ca9b434986156eacaa7b16b5dbbd5
AbiMask <abisubimask@gmail.com>
2025-06-19 06:36:08 +0530

    June 19

---
+++ src/ex_10_For_Loop/Lab099_For_Loop_Var_Name.java
@@ -0,0 +1,9 @@
+package ex_10_For_Loop;
+
+public class Lab099_For_Loop_Var_Name {
+    public static void main(String[] args) {
+        for(int abi=0; abi<10 ; abi++){
+            System.out.println(abi);
+        }
+    }
+}
