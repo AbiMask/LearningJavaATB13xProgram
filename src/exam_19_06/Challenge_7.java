c9f4d6e0980ba5b6271020fe376c5be4bb9b57bf
AbiMask <143061178+AbiMask@users.noreply.github.com>
2025-06-19 08:19:58 +0530

    Update Challenge_7.java

--- src/exam_19_06/Challenge_7.java
+++ src/exam_19_06/Challenge_7.java
@@ -1,4 +1,22 @@
 package exam_19_06;
 
 public class Challenge_7 {
+  Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Var 1: ");
+        int var1 = sc.nextInt();
+        System.out.println("Enter Var 2: ");
+        int var2 = sc.nextInt();
+        System.out.println("Enter Var 3: ");
+        int var3 = sc.nextInt();
+
+        if(var1>var2 && var1>var3){
+            System.out.println("Largest number is: "+var1);
+        }
+        else if(var2>var3){
+          System.out.println("Largest number is: "+var2);
+        }
+        else{
+          System.out.println("Largest number is: "+var3);
+        }
+
 }
