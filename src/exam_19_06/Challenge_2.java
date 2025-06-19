29afe0b5ea9e964af1fe6be7b0eb53789fcc3167
AbiMask <abisubimask@gmail.com>
2025-06-19 07:46:25 +0530

    June 19

--- src/exam_19_06/Challenge_2.java
+++ src/exam_19_06/Challenge_2.java
@@ -1,7 +1,27 @@
 package exam_19_06;
 
+import java.util.Scanner;
+
 public class Challenge_2 {
     public static void main(String[] args) {
+        /*
+        * **Requirements:**
+        - Use +, -, *, /, % operators
+        - Demonstrate assignment operators (=, +=, -=, *=, /=)
+        - Show the results of each operation
+        * */
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Var 1: ");
+        int var1 = sc.nextInt();
+        System.out.println("Enter Var 2: ");
+        int var2 = sc.nextInt();
+
+        System.out.println("Addition: "+ (var1+var2));
+        System.out.println("Subtraction: "+ (var1-var2));
+        System.out.println("Multiplication: "+ (var1*var2));
+        System.out.println("Division: "+ (var1/var2));
+        System.out.println("Modulus: "+ (var1%var2));
 
     }
 }
