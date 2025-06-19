d9d3037756ee7f0ebb9ddf7bcda49bda154fbf9f
AbiMask <abisubimask@gmail.com>
2025-06-19 08:10:39 +0530

    June 19

---
+++ src/exam_19_06/Challenge_6.java
@@ -0,0 +1,26 @@
+package exam_19_06;
+
+import java.util.Scanner;
+
+public class Challenge_6 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Var 1: ");
+        int var1 = sc.nextInt();
+
+        if(var1 == 0){
+            System.out.println(var1+" is zero");
+        }
+        else if (var1>0) {
+            System.out.println(var1+" is positive");
+        }
+        else if (var1<0) {
+            System.out.println(var1+" is negative");
+        }
+        else{
+            System.out.println("Incorrect Input");
+        }
+    }
+
+}
