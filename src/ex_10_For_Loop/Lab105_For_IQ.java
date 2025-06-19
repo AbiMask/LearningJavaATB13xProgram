9cd50c48bd625026675ad8b431a6d015087b3551
AbiMask <abisubimask@gmail.com>
2025-06-19 06:56:05 +0530

    June 19

---
+++ src/ex_10_For_Loop/Lab105_For_IQ.java
@@ -0,0 +1,11 @@
+package ex_10_For_Loop;
+
+public class Lab105_For_IQ {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; ) {
+            System.out.println("Hello " + i);
+        }
+
+        // for loop without increment will execute the program infinite if the condition met with initial value
+    }
+}
