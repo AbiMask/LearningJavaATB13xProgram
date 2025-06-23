78cf7e2d74b9fb0e54ac14beb274c89e63479d0e
AbiMask <abisubimask@gmail.com>
2025-06-23 10:48:18 +0530

    June 23

--- src/task_23_06/Palindrome_String.java
+++ src/task_23_06/Palindrome_String.java
@@ -5,14 +5,14 @@ public class Palindrome_String {
     public static void main(String[] args) {
 
         String name = "Madam";
-        if(check_Palindromw(name))
+        if(check_Palindrome_Rev(name))
             System.out.println(name + " is a Palindrome");
         else
             System.out.println(name + " is not a Palindrome");
 
     }
 
-    public static boolean check_Palindromw(String name){
+    public static boolean check_Palindrome_Two_Pointers(String name){
         name = name.toLowerCase();
         boolean isPalindrome = true;
         int j = name.length()-1;
@@ -26,4 +26,18 @@ public class Palindrome_String {
 
         return true;
     }
+
+    public static boolean check_Palindrome_Rev(String name){
+        System.out.println("Entered into loop");
+        String rev = new String();
+        for(int i=name.length()-1; i>=0; i--){
+            rev = rev +name.charAt(i);
+            System.out.println(rev);
+        }
+        if(name.equalsIgnoreCase(rev))
+            return true;
+        else
+            return  false;
+    }
+
 }
