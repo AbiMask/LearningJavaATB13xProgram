1593db2a519d34526aa3228a28ccacb7e25357ab
AbiMask <abisubimask@gmail.com>
2025-06-24 11:11:22 +0530

    June 24

---
+++ src/task_24_06/Alphabet_Check.java
@@ -0,0 +1,23 @@
+package task_24_06;
+
+import java.util.Scanner;
+
+public class Alphabet_Check {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a single character: ");
+        String input = sc.next();
+
+        if (input.length() == 1) {
+            char alphabet = input.charAt(0);
+            int asci = (int) alphabet;
+            if ((asci >= 65 && asci <= 90) || (asci >= 97 && asci <= 122)) {
+                System.out.println(alphabet + " is an alphabet");
+            } else {
+                System.out.println(alphabet + " is not an alphabet");
+            }
+        } else {
+            System.out.println("Please enter only a single character.");
+        }
+    }
+}
