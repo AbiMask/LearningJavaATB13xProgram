40267031e07c49dd50cdcc5469f5fe6828b3fde2
AbiMask <abisubimask@gmail.com>
2025-06-24 11:48:44 +0530

    June 24

---
+++ src/task_24_06/Triangle_Side_Check.java
@@ -0,0 +1,22 @@
+package task_24_06;
+
+import java.util.Scanner;
+
+public class Triangle_Side_Check {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the three sides of triangle: ");
+        System.out.println("Side 1: ");
+        int side1 = sc.nextInt();
+        System.out.println("Side 2: ");
+        int side2 = sc.nextInt();
+        System.out.println("Side 3: ");
+        int side3 = sc.nextInt();
+
+        if( (side1+side2)>side3 && (side2+side3)>side1 && (side3+side1)>side2)
+            System.out.println("Valid Triangle");
+        else
+            System.out.println("Invalid Triangle");
+    }
+}
