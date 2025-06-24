dba2a4c62829f7d3497dfa8ff00f257a407b42b3
AbiMask <abisubimask@gmail.com>
2025-06-24 10:46:55 +0530

    June 24

---
+++ src/task_24_06/Min_Three_Number.java
@@ -0,0 +1,26 @@
+package task_24_06;
+
+import java.util.Scanner;
+
+public class Min_Three_Number {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Var 1: ");
+        int var1 = sc.nextInt();
+        System.out.println("Enter Var 2: ");
+        int var2 = sc.nextInt();
+        System.out.println("Enter Var 3: ");
+        int var3 = sc.nextInt();
+
+        if(var1<=var2 && var1<=var3){
+            System.out.println("Smallest number is: "+var1);
+        }
+        else if(var2<=var3){
+            System.out.println("Smallest number is: "+var2);
+        }
+        else{
+            System.out.println("Smallest number is: "+var3);
+        }
+    }
+}
+
