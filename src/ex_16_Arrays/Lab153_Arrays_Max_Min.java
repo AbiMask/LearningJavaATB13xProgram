5d08b3f63a7ad2819b355084ec542e39b5726b2d
AbiMask <abisubimask@gmail.com>
2025-07-10 08:06:36 +0530

    July 10

--- src/ex_16_Arrays/Lab153_Arrays_Max_Min.java
+++ src/ex_16_Arrays/Lab153_Arrays_Max_Min.java
@@ -1,7 +1,41 @@
 package ex_16_Arrays;
 
+import java.util.Arrays;
+
 public class Lab153_Arrays_Max_Min {
     public static void main(String[] args) {
 
+        int[] arr = {234,1,13,14,14,5245,245};
+
+        /*
+        Arrays.sort(arr);
+        System.out.println("Min: "+arr[0]);
+        System.out.println("Max: "+arr[arr.length-1]);
+         */
+
+        int min_value = get_min(arr);
+        int max_value = get_max(arr);
+        System.out.println("Min: "+min_value);
+        System.out.println("Max: "+max_value);
+    }
+
+    static int get_min(int[] arr){
+        int min = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i]<min)
+                min = arr[i];
+        }
+
+        return min;
+    }
+
+    static int get_max(int[] arr){
+        int max = arr[0];
+        for(int i=1; i<arr.length; i++){
+            if(arr[i] > max)
+                 max = arr[i];
+        }
+
+        return max;
     }
 }
