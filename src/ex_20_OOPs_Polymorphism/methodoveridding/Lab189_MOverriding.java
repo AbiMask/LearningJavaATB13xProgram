63329d80dbab9f76a0a42d2efaea205d552c73bd
AbiMask <abisubimask@gmail.com>
2025-07-14 23:08:06 +0530

    July 14

---
+++ src/ex_20_OOPs_Polymorphism/methodoveridding/Lab189_MOverriding.java
@@ -0,0 +1,31 @@
+package ex_20_OOPs_Polymorphism.methodoveridding;
+
+import java.util.function.DoublePredicate;
+
+public class Lab189_MOverriding {
+    public static void main(String[] args) {
+
+
+        Dog dog = new Dog();
+        dog.sound();
+    }
+}
+
+class Animal{
+    void sound(){
+        System.out.println("Animals makes sounds !!");
+    }
+}
+
+class Dog extends Animal{
+    @Override
+    void sound(){
+        System.out.println("Dog Barks !!");
+    }
+
+    void methodOverLoad(){
+    }
+
+    void methodOverLoad(int a){
+    }
+}
